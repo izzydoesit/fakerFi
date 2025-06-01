11ebdd105355839d067167ddeb731824717acc4d
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-06-01 14:33:13 -0400

    Add default items arg and empty items check in useMemo func inside usePagination hook

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,14 +1,14 @@
 import { useMemo, useState } from "react";
 
-export function usePagination<T>(items: T[], pageSize = 5) {
+export function usePagination<T>(items: T[] = [], pageSize = 5) {
   const [page, setPage] = useState(1);
 
   const totalPages = Math.ceil(items.length / pageSize);
 
-  const currentPageItems = useMemo(
-    () => items.slice((page - 1) * pageSize, page * pageSize),
-    [items, page, pageSize]
-  );
+  const currentPageItems = useMemo(() => {
+    if (!items || items.length === 0) return [];
+    return items.slice((page - 1) * pageSize, page * pageSize);
+  }, [items, page, pageSize]);
 
   return {
     currentPageItems,
