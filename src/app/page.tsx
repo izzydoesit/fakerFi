fb36aac9ed395f67a661d2164ac7c6cad0d0ea20
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-06-01 19:34:41 -0400

    Add type import to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Dashboard from "@/components/Dashboard";
 import { Metadata } from "next";
+import type { JSX } from "react";
 
 export const metadata: Metadata = {
   title: "FakerFi Dashboard",
